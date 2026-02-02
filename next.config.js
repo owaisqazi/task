bf9eac0a34db75da9d95e61f0995c6229d4aa6fa
owaisqazi <waqqasqaziqazi@gmail.com>
2026-02-02 16:04:13 +0500

    first commit

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  // Tell Next.js your source directory
-  // This tells Next to look inside `src` folder for pages/app
   srcDir: 'src',
 };
 
