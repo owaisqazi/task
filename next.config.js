958bac2d2a3a169d501a12ddabba378e884caf56
owaisqazi <waqqasqaziqazi@gmail.com>
2026-02-02 16:06:41 +0500

    first commit

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  srcDir: 'src',
+  srcDir: 'src', // important to tell Next.js to look inside src
 };
 
 module.exports = nextConfig;
